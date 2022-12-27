619a4eb1a529aec19a794fdde9f27bb406f6e4f6
santiago-semb <sansembbb@gmail.com>
2022-12-27 11:53:15 -0300

    maqueta-angular

--- proyectoIntegradorArgPrograma/src/app/app.module.ts
+++ proyectoIntegradorArgPrograma/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { SubNavComponent } from './sub-nav/sub-nav.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    InicioComponent,
+    ProyectosComponent,
+    LoginComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    SubNavComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
